b864e35c5eeae803376cc11a265901e50d175976
Paolo Carta <ing.pcarta@gmail.com>
2018-08-30 15:45:14 +0200

    refactoring into an actual unit test

--- employee-service/src/test/java/com/rabbit/samples/employeeservice/unittests/services/EmployeeServiceUnitTest.java
+++ employee-service/src/test/java/com/rabbit/samples/employeeservice/unittests/services/EmployeeServiceUnitTest.java
@@ -4,11 +4,15 @@ import com.rabbit.samples.employeeservice.persistence.entities.Employee;
 import com.rabbit.samples.employeeservice.persistence.repos.EmployeeRepository;
 import com.rabbit.samples.employeeservice.services.EmployeeService;
 import com.rabbit.samples.employeeservice.services.impl.EmployeeServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,62 +20,36 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
-@IfProfileValue(name = "spring.testing.profile", values = {"all", "unit"})
-@RunWith(SpringRunner.class)
+
+@RunWith(MockitoJUnitRunner.class)
 public class EmployeeServiceUnitTest {
 
-	/*
-		To check the Service class, we need to have an instance of Service class created and available as a @Bean so that we can @Autowire it in our test class.
-		This configuration is achieved by using the @TestConfiguration annotation.
+	@InjectMocks
+	private EmployeeServiceImpl employeeService;
 
-		PLEASE NOTE:
-		During component scanning, we might find components or configurations created only for specific tests accidentally get picked up everywhere.
-		To help prevent that, Spring Boot provides @TestConfiguration annotation that can be used on classes in src/test/java to indicate that they should
-		not be picked up by scanning.
-	 */
-	@TestConfiguration
-	@Import(EmployeeServiceImpl.class)
-	static class EmployeeServiceUnitTestContextConfiguration {
-
-		// no-op
-	}
-
-
-	@Autowired
-	private EmployeeService employeeService;
-
-	/*
-		Mocking support provided by Spring Boot Test: it creates a Mock for the EmployeeRepository which can be used to bypass the call to the actual
-		EmployeeRepository.
-
-		PLEASE NOTE:
-		In the EmployeeService is specified a @Qualifier for the EmployeeRepository injection, that refers to a bean named "employeeRepository".
-		During the test execution, the EmployeeRepository bean is mocked with a name different than the variable name (something like
-		com.rabbit.samples.employeeservice.unittests.services.EmployeeRepository#0), leading to miss the EmployeeService injection.
-	 */
-	@MockBean(name = "employeeRepository")
+	@Mock
 	private EmployeeRepository employeeRepository;
 
 	final String name = "alex";
 
 	@Test
-	public void givenValidName_thenReturnFoundEmployee() throws Exception {
+	public void givenValidName_thenReturnFoundEmployee() {
 
 		// given
-		BDDMockito
-				.when(employeeRepository.findByName(name))
+		when(employeeRepository.findByName(name))
 				.thenReturn(Employee.builder().name(name).build());
 
 		// when
 		final Employee found = employeeService.getByName(name);
 
 		// then
-		Assert.assertEquals(name, found.getName());
+		assertThat(name).isEqualTo(found.getName());
 
 		// verify
-		BDDMockito.verify(employeeRepository, VerificationModeFactory.times(1)).findByName(name);
-		BDDMockito.reset(employeeRepository);
+		verify(employeeRepository, VerificationModeFactory.times(1)).findByName(name);
 	}
 
 }
